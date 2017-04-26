a852db3cf56d5c8f931f081d18558969523e05aa
Evgeny Karasev <gnomskg@gmail.com>
2017-04-26 16:03:43 +0300

    Start working with page components

---
+++ src/components/ContactGroup/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function ContactGroup() {
+}
+
+export default ContactGroup;
---
+++ src/components/ContactList/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ContactList(props) {
+  return (
+    <div className="">
+
+    </div>
+  )
+}
+
+export default ContactList;
